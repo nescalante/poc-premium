df68c29c87e8dc3235401276497a14dd348aaacc
Nicolás Escalante <nlante@gmail.com>
2014-10-27 12:19:57 -0300

    layout changes

--- models/premium/index.js
+++ models/premium/index.js
@@ -18,37 +18,7 @@ function ContractPremium() {
     return new Month(i, self);
   }).forEach(addMonth);
 
-  self.take = function (count) {
-    return function(property) {
-      return ko.computed(take.bind(self, property, count));
-    };
-  };
-
   function addMonth(month) {
     self.months.push(month);
   }
-}
-
-function take(property, count) {
-  var current = 0;
-  var result = [];
-
-  property().forEach(function (i, ix) {
-    var last = result.length - 1;
-    current++;
-
-    if (!result[last]) {
-      result.push([]);
-      last++;
-    }
-
-    if (result[last].length === count) {
-      result.push([]);
-      last++;
-    }
-
-    result[last].push(i);
-  });
-
-  return result;
 }
